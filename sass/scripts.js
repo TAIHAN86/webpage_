10ff580a20e3cfb7a660360a1b7e9d16be681577
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-09-22 23:33:23 +0530

    added comments

--- sass/scripts.js
+++ sass/scripts.js
@@ -1,4 +1,5 @@
-function openNav() {
+   // (navbar js code))
+ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
